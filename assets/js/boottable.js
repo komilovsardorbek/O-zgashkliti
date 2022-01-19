d8e80f3b6c060865aafd1b3376f47321502afbc5
komilovsardorbek <sardork435@gmail.com>
2022-01-19 07:32:35 +0500

    root

--- assets/js/boottable.js
+++ assets/js/boottable.js
@@ -8,6 +8,7 @@ function childrenRow() {
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
         'id="addrow" onclick="childrenRow()"\n' + '/></td>\n' +'</td></tr>');
 }
@@ -16,6 +17,7 @@ var j = 1;
 function childrenTable() {
     j++;
     $('#childTable1').find('tbody').append('<tr><th scope="row">' + j + '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
@@ -41,4 +43,19 @@ function childrentable() {
         '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
         '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
         'id="addrow1" onclick="childrentable()"\n' + '/></td>\n' +'</td></tr>');
+}
+
+var x = 1;
+function childrentabl1() {
+    x++;
+    $('#childTable3').find('tbody').append('<tr><th scope="row">' + x +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow3" onclick="childrentabl1()"\n' + '/></td>\n' +'</td></tr>');
 }
